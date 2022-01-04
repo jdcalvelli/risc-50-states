e789e123b7d13c6a996f04562c4af482fc7880dd
JD Calvelli <jdcalvelli@gmail.com>
2022-01-04 16:22:23 -0600

    remove boilerplate html/css/js, remove js bc it breaks layout for some reason

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-import anime from 'animejs';
 import fullpage from 'fullpage.js';
+import anime from 'animejs';
 
-import '/node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+import '/node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 import './style.css';
-
-// document.querySelector('#app').innerHTML = `
-//   <h1>Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-// `
