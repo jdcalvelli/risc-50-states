a8208f1defce3e65673d8153492b77758c65322e
JD Calvelli <jdcalvelli@gmail.com>
2022-01-04 15:27:17 -0600

    import fontawesome css, fullpagejs, animejs in main.js using es6 import

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-import './style.css'
+import './node_modules/@fortawesome/fontawesome-free/css/all.css';
+import './style.css';
+
+import fullpagejs from './node_modules/fullpage.js/dist/fullpage.min.js';
+import anime from 'animejs/lib/anime.es.js';
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
