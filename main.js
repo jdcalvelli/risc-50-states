1cde421dd3a0bbf86d16d3c37d501a4be1e36d96
JD Calvelli <jdcalvelli@gmail.com>
2022-01-04 16:03:12 -0600

    add html, css, and update javascript imports so they work

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import './node_modules/@fortawesome/fontawesome-free/css/all.css';
+import anime from 'animejs';
+import fullpage from 'fullpage.js';
+
+import '/node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 import './style.css';
 
-import fullpagejs from './node_modules/fullpage.js/dist/fullpage.min.js';
-import anime from 'animejs/lib/anime.es.js';
-
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
