16dd5c51455aeab2e589157ba0cc48a4bd5b8918
JD Calvelli <jdcalvelli@gmail.com>
2022-01-05 00:42:17 -0600

    add animations from animejs for each fontawesome image

--- main.js
+++ main.js
@@ -6,4 +6,43 @@ import './style.css';
 
 $(document).ready(function() {
 
+    //anime js for each image
+    anime({
+        targets: ".fa-globe-americas",
+        opacity: 1,
+        duration: 2000,
+        easing: 'easeInOutSine',
+    });
+    anime({
+        targets: ".fa-chart-area",
+        opacity: 1,
+        duration: 2000,
+        easing: 'easeInOutSine',
+      });
+    anime({
+        targets: ".fa-lightbulb",
+        opacity: 1,
+        duration: 2000,
+        easing: 'easeInOutSine',
+    });
+    anime({
+        targets: ".fa-graduation-cap",
+        opacity: 1,
+        duration: 2000,
+        easing: 'easeInOutSine',
+    });
+    anime({
+        targets: ".fa-flag-usa",
+        opacity: 1,
+        duration: 2000,
+        easing: 'easeInOutSine',
+    });
+    anime({
+        targets: "#risc-logo",
+        opacity: 1,
+        duration: 2000,
+        easing: 'easeInOutSine',
+    });
+    
+
 });
