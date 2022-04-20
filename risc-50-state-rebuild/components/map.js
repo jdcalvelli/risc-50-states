892fc33fa02e71714e7f83f84bab742779ffa88d
JD Calvelli <jdcalvelli@gmail.com>
2022-04-20 11:51:33 -0500

    implement mithril-leaflet package for map display

--- risc-50-state-rebuild/components/map.js
+++ risc-50-state-rebuild/components/map.js
@@ -2,12 +2,24 @@
 //put the map in a div and have the inside be the map
 //how to track across this component to the other with the data? - global variable?
 import L from 'leaflet';
-import provider from 'leaflet-providers';
+import { LeafletMap } from 'mithril-leaflet';
 import m from 'mithril';
+
 
 const Map = {
     view: function() {
-        return m('h1', 'this is where the map needs to go')
+        return m(LeafletMap, {
+            style: 'height: 400px; margin-top: 20px;',
+            baseLayers: {
+                osm: {
+                    url: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png',
+                    options: {
+                        subdomains: ['a', 'b', 'c', 'd'],
+                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+                    }
+                }
+            }
+        })
     }
 }
 
