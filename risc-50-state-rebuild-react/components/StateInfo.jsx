d5f466c62af2c8f94d7be44bde76882869748b63
JD Calvelli <jdcalvelli@gmail.com>
2022-04-25 12:20:05 -0500

    add StateInfo component

---
+++ risc-50-state-rebuild-react/components/StateInfo.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+//no state changes, just shows info based on state set through map click
+//on click of a particular state in MapComponent, pass which state got clicked down as props to StateInfo
+//display information about that state
+
+function StateInfo(props) {
+    return (
+        <div className="stateInfo">
+            <h3>state name: state name will go here </h3>
+
+            <h5>analysis: </h5>
+            <p>this is where analysis will go</p>
+
+            <p>see current math standards here - link</p>
+            <p>see current computer science standards here - link</p>
+            <p>see current career and tech ed sequence here - link</p>
+            <p>next general science standards adopted? </p>
+        </div>
+    )
+}
+
+export { StateInfo }
--- risc-50-state-rebuild-react/components/map.jsx
+++ risc-50-state-rebuild-react/components/map.jsx
@@ -1,6 +1,7 @@
-import { Renderer } from "leaflet";
 import React from "react";
 import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+
+import { StateInfo } from "./StateInfo";
 
 //this will be the leaflet map - which will need state i believe?
 //has to be functional component - meaning the state will have to be lifted from here
@@ -8,13 +9,20 @@ import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 
 function MapSection(props) {
     return (
-        <MapContainer style={{height: 600, width: 900}} center={[37.0902, -95.7129]} zoom={4}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            
-        </MapContainer>
+        <div className="mapSection">
+
+            {/* actual drawing of leaflet map */}
+            <MapContainer style={{ height: 600, width: 900 }} center={[37.0902, -95.7129]} zoom={4}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+
+            </MapContainer>
+
+            {/* implementation of state info section */}
+            <StateInfo/>
+        </div>
     )
 }
 
