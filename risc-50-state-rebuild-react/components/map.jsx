853084e4944ce569b559078ecfa45aadd40e2aa7
JD Calvelli <jdcalvelli@gmail.com>
2022-04-25 12:07:47 -0500

    moving height and width declaration of map into map component

--- risc-50-state-rebuild-react/components/map.jsx
+++ risc-50-state-rebuild-react/components/map.jsx
@@ -4,11 +4,11 @@ import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 
 //this will be the leaflet map - which will need state i believe?
 //has to be functional component - meaning the state will have to be lifted from here
-//also need to set an explicit size for leaflet container in the css, otherwise breaks
+// css moved to in the container declaration, out of css file
 
 function MapSection(props) {
     return (
-        <MapContainer center={[37.0902, -95.7129]} zoom={4}>
+        <MapContainer style={{height: 600, width: 900}} center={[37.0902, -95.7129]} zoom={4}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
