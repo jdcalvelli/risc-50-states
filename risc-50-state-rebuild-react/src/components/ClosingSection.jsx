822c099f08ce5fb5019e3e0d1f43e65faef1632b
JD Calvelli <jdcalvelli@gmail.com>
2022-04-27 15:22:23 -0500

    adding mailto link for risc email

--- risc-50-state-rebuild-react/src/components/ClosingSection.jsx
+++ risc-50-state-rebuild-react/src/components/ClosingSection.jsx
@@ -8,7 +8,7 @@ function ClosingSection(props) {
             <p><span style={{fontWeight: 'bold'}}>See a bad grade for your state?</span> Take a look at our <a href="https://docs.google.com/document/d/1GompsDPMiYgzp0dZ_0tYuXJKQFexkQXA3Cc3-Qflvus/edit?usp=sharing">advocacy pack</a>, and consider calling your state education department to find out more. </p>
 
             <p><span style={{fontWeight:'bold'}}>Did we miss something?</span> Disagree with our grade? 
-            Get in touch with us at centerforrisc@gmail.com.</p>
+            Get in touch with us at <a href="mailto:centerforrisc@gmail.com?subject=50 State Snapshot Feedback">centerforrisc@gmail.com.</a></p>
             
             <p>This map is a work in progress, and we are trying our best to keep pace with all the momentum in the field. 
                 Ultimately, this work is meant <span style={{fontWeight: 'bold'}}>to motivate more action, 
