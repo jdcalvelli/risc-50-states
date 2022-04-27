db36df5ecf2c8c10d38ee134a3ef46c979c9e9ea
JD Calvelli <jdcalvelli@gmail.com>
2022-04-27 15:10:53 -0500

    fix typo on usemaphelper component

--- risc-50-state-rebuild-react/src/components/helperComponents/UseMapHelper.jsx
+++ risc-50-state-rebuild-react/src/components/helperComponents/UseMapHelper.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 function UseMapHelper(props) {
     return(
         <div className='stateInfo'>
-            <h1>Choose a state on the map to learn more about the Center for RISC's thinks it's doing.</h1>
+            <h1>Choose a state on the map to learn more about how the Center for RISC's thinks it's doing.</h1>
         </div>
     ) 
 }
