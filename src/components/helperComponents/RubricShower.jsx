e9fc5cc338f955725d3a36d9d39450b517e25bef
JD Calvelli <jdcalvelli@gmail.com>
2022-04-28 10:42:13 -0500

    add styling for rubricShower w sneaky duo part color shift

--- src/components/helperComponents/RubricShower.jsx
+++ src/components/helperComponents/RubricShower.jsx
@@ -5,7 +5,9 @@ import React from "react";
 function RubricShower(props) {
     return (
         <div className="rubricShower" onClick={props.handleClick}>
-            <h3>see the full rubric</h3>
+            <div className="rubricShowerInner">
+                <h3>see the full rubric</h3>
+            </div>
         </div>
     )
 }
