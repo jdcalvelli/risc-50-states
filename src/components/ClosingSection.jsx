e485521112546c88c66d3a6034f4a6908c3672f2
JD Calvelli <jdcalvelli@gmail.com>
2022-05-02 17:09:11 -0500

    add on site reach out to april and i

--- src/components/ClosingSection.jsx
+++ src/components/ClosingSection.jsx
@@ -10,8 +10,8 @@ function ClosingSection(props) {
             <p><span style={{fontWeight: 'bold'}}>See a bad grade for your state?</span> Take a look at our <a href="https://docs.google.com/document/d/1GompsDPMiYgzp0dZ_0tYuXJKQFexkQXA3Cc3-Qflvus/edit?usp=sharing">advocacy pack</a>, and consider calling your state education department to find out more. </p>
 
             <p><span style={{fontWeight:'bold'}}>Did we miss something?</span> Disagree with our grade? 
-            Get in touch with us at <a href="mailto:centerforrisc@gmail.com?subject=50 State Snapshot Feedback">centerforrisc@gmail.com.</a></p>
-            
+            Get in touch with us directly at <br/> <a href="mailto:aprilfeng@uchicago.edu?subject=50 State Snapshot Feedback">aprilfeng@uchicago.edu</a> or <a href="mailto:jdcalvelli@uchicago.edu?subject=50 State Snapshot Feedback">jdcalvelli@uchicago.edu</a>.</p>
+
             <p>This map is a work in progress, and we are trying our best to keep pace with all the momentum in the field. <br/>
                 Ultimately, this work is meant <span style={{fontWeight: 'bold'}}>to motivate more action, 
                 not to hurt feelings.</span> <br/> Many states are charging ahead – we hope they can use this work as a 
