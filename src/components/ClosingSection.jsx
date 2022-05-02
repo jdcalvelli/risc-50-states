f12b0d606e977d319889740b48fe90bd078938f6
JD Calvelli <jdcalvelli@gmail.com>
2022-05-02 17:27:40 -0500

    change from email displayed on site to name displayed on site

--- src/components/ClosingSection.jsx
+++ src/components/ClosingSection.jsx
@@ -10,7 +10,7 @@ function ClosingSection(props) {
             <p><span style={{fontWeight: 'bold'}}>See a bad grade for your state?</span> Take a look at our <a href="https://docs.google.com/document/d/1GompsDPMiYgzp0dZ_0tYuXJKQFexkQXA3Cc3-Qflvus/edit?usp=sharing">advocacy pack</a>, and consider calling your state education department to find out more. </p>
 
             <p><span style={{fontWeight:'bold'}}>Did we miss something?</span> Disagree with our grade? 
-            Get in touch with us directly at <br/> <a href="mailto:aprilfeng@uchicago.edu?subject=50 State Snapshot Feedback">aprilfeng@uchicago.edu</a> or <a href="mailto:jdcalvelli@uchicago.edu?subject=50 State Snapshot Feedback">jdcalvelli@uchicago.edu</a>.</p>
+            Get in touch with us directly: <br/> We're <a href="mailto:aprilfeng@uchicago.edu?subject=50 State Snapshot Feedback">April Feng</a> and <a href="mailto:jdcalvelli@uchicago.edu?subject=50 State Snapshot Feedback">JD Calvelli</a> at <a href="mailto:centerforrisc@gmail.com?subject=50 State Snapshot Feedback">The Center for RISC</a>.</p>
 
             <p>This map is a work in progress, and we are trying our best to keep pace with all the momentum in the field. <br/>
                 Ultimately, this work is meant <span style={{fontWeight: 'bold'}}>to motivate more action, 
