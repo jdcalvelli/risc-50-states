6fbad44f58f5e4f9314643737fa4193a616029fa
JD Calvelli <jdcalvelli@gmail.com>
2022-05-09 14:56:26 -0500

    adding first pass navbar mobile and laptop responsive

--- src/components/NavBarSection.jsx
+++ src/components/NavBarSection.jsx
@@ -7,7 +7,7 @@ function NavBarSection(props) {
         <div className="navBarSection">
             {/* SVG IMAGE FOR LOGO */}
             <a href="https://risc.uchicago.edu"><img src="src/assets/risc_LOGO.svg" alt="RISC Logo" /></a>
-            <h3>Back to RISC</h3>
+            <a href="https://risc.uchicago.edu"><h3 style={{fontWeight: 'bold'}}>Back to RISC</h3></a>
         </div>
     )
 }
