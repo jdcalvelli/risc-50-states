460010f605958e92ed80b6e7917aa8fa97a9cf5b
JD Calvelli <jdcalvelli@gmail.com>
2022-05-11 15:25:04 -0500

    adding br in first paragraph of closing section for better readability

--- src/components/ClosingSection.jsx
+++ src/components/ClosingSection.jsx
@@ -7,15 +7,12 @@ function ClosingSection(props) {
         <div className="closingSection">
             <h3>Take Action</h3>
 
-            <p><span style={{fontWeight: 'bold'}}>See a bad grade for your state?</span> Take a look at our <a href="https://docs.google.com/document/d/1GompsDPMiYgzp0dZ_0tYuXJKQFexkQXA3Cc3-Qflvus/edit?usp=sharing">advocacy pack</a>, and consider <br/> calling your state education department to find out more. </p>
+            <p><span style={{fontWeight: 'bold'}}>See a bad grade for your state?</span> Take a look at our <a href="https://docs.google.com/document/d/1GompsDPMiYgzp0dZ_0tYuXJKQFexkQXA3Cc3-Qflvus/edit?usp=sharing">advocacy pack</a>, <br/> and consider calling your state education department to find out more. </p>
 
             <p><span style={{fontWeight:'bold'}}>Did we miss something?</span> Disagree with our grade? <br/>
             Get in touch with us directly at <a href="mailto:centerforrisc@gmail.com?subject=50 State Snapshot Feedback">centerforrisc@gmail.com</a>.</p>
 
-            <p>This map is a work in progress, and we are trying our best <br/> to keep pace with all the momentum in the field.</p>
-
-            <p>Ultimately, this work is meant <span style={{fontWeight: 'bold'}}>to motivate more action, not to hurt feelings.</span> <br/>
-             Many states are charging ahead – we hope they can use this work as a resource to do more. </p>
+            <p>This map is a work in progress, and we are trying our best to keep pace with all the momentum in the field. Ultimately, this work is meant <span style={{fontWeight: 'bold'}}>to motivate more action, not to hurt feelings.</span> Many states are charging ahead – we hope they can use this work as a resource to do more. </p>
 
         </div>
     )
