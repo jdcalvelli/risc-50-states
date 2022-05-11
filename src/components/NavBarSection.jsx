024c9c8b3af2383505ef16c774d332386ecc56c6
JD Calvelli <jdcalvelli@gmail.com>
2022-05-11 15:15:16 -0500

    changing svg import so it shows up in the production build

--- src/components/NavBarSection.jsx
+++ src/components/NavBarSection.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+import imgUrl from '../assets/risc_LOGO.svg'
+
 // stateless nav bar with risc logo and back to risc area
 
 function NavBarSection(props) {
     return (
         <div className="navBarSection">
             {/* SVG IMAGE FOR LOGO */}
-            <a href="https://risc.uchicago.edu"><img src="src/assets/risc_LOGO.svg" alt="RISC Logo" /></a>
+            <a href="https://risc.uchicago.edu"><img src={imgUrl} alt="RISC Logo" /></a>
             <a href="https://risc.uchicago.edu"><h3 style={{fontWeight: 'bold'}}>Back to RISC</h3></a>
         </div>
     )
